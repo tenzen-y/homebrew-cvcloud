d9746dbea9a9a7cf6c82687e7b6af755ee060810
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-27 06:48:38 +0900

    Brew formula update for cvcloud version v1.1.1

--- cvcloud.rb
+++ cvcloud.rb
@@ -6,16 +6,16 @@ require_relative "./lib/download_strategy.rb"
 class Cvcloud < Formula
   desc "client tool to use cvcloud"
   homepage "https://gitlab.com/kindaicvlab/cvcloud/cvcloud"
-  version "1.1.0"
+  version "1.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/tenzen-y/cvcloud/releases/download/v1.1.0/cvcloud_1.1.0_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "cf6e572f7d435204bf650e5718efb48d335274b5c2868f12ddf18620222807d9"
+    url "https://github.com/tenzen-y/cvcloud/releases/download/v1.1.1/cvcloud_1.1.1_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "b5c63fa4ffd51b761a5914b6ae2d0db10315df9cfb49927ced4449815cf0ea6e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tenzen-y/cvcloud/releases/download/v1.1.0/cvcloud_1.1.0_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "b3c497da8dba2450ab74e483f327102558c51ed1a945fd89e70f232887f9d9f4"
+    url "https://github.com/tenzen-y/cvcloud/releases/download/v1.1.1/cvcloud_1.1.1_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "a676c86b19633ca94c881f73a458fd3686c1664fa443eb83bc295651337011da"
   end
 
   def install
